3582ba9692895f71ed24ad9bd5fbe0b524eaaf36
Vineet jain <85371364+2001vineet@users.noreply.github.com>
2024-04-25 17:30:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$('.carousel').carousel({
+    interval: 3000, // Adjust the interval duration as needed (in milliseconds)
+    pause: 'hover', // Pause on hover
+    wrap: true // Wrap around to the first slide after reaching the last slide
+  });
+
+
+  
